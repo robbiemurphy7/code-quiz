eeb673ee4e07851f24f1f78b0b3a03ebc3c2085b
robbiemurphy <rmurphy987@gmail.com>
2022-01-14 16:00:26 -0600

    html landing page created and timer javascript implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# code-quiz
+# Coding Quiz
+
+##
---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+
+// TIMER
+var minutesLabel = document.querySelector("#minutes");
+var secondsLabel = document.querySelector("#seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length <2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
